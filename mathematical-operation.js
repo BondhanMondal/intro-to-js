a563eb1156883825dd7012c45d4ab3e2b8f1b61b
Bondhan Mondal <bondhan2725@gmail.com>
2022-01-26 19:59:20 +0600

    lesson complete

--- mathematical-operation.js
+++ mathematical-operation.js
@@ -19,4 +19,21 @@ var moneyAvailable = 100;
 var orangePrice = 5;
 var orangeQuantity = moneyAvailable / orangePrice;
 
-console.log(orangeQuantity);
+// console.log(orangeQuantity);
+
+// special eddition 
+var fastNumber = 0.1;
+var secondNumber = 0.2;
+var total = fastNumber + secondNumber;
+total = total.toFixed(1);
+total = parseFloat(total);
+// console.log(parseFloat(total.toFixed(3)));
+// console.log(total);
+
+// modulus 
+var mangoes = 57;
+var hungryPerson = 12;
+var dividing = mangoes / hungryPerson;
+var remaining = mangoes % hungryPerson;
+console.log(remaining);
+console.log(dividing);
