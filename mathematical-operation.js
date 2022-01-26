b15960e89cbe0abf9edc12ec6191fcbbc6ab1362
Bondhan Mondal <bondhan2725@gmail.com>
2022-01-26 18:00:20 +0600

    simple mathematic operation done

---
+++ mathematical-operation.js
@@ -0,0 +1,22 @@
+var onionPrice = 43;
+var eggPrice = 42;
+var eggQuantity = 7;
+
+var totalPrice = onionPrice + eggPrice;
+var priceDifference = onionPrice - eggPrice;
+var totalEggPrice = eggPrice * eggQuantity;
+
+
+// console.log(onionPrice);
+// console.log(eggPrice);
+// console.log(onionPrice + eggPrice);
+// console.log(totalPrice);
+
+// console.log(priceDifference);
+// console.log(totalEggPrice);
+
+var moneyAvailable = 100;
+var orangePrice = 5;
+var orangeQuantity = moneyAvailable / orangePrice;
+
+console.log(orangeQuantity);
