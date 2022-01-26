4b3434cccf43f4824e1b8a926bab92294be95a9e
Bondhan Mondal <bondhan2725@gmail.com>
2022-01-26 17:27:26 +0600

    variable data type done

--- variable.js
+++ variable.js
@@ -5,3 +5,12 @@ var birthyear = 1994;
 var a = 200;
 var gender = 2;
 var money = 15000;
+
+var name = "sodor uddin";
+var name2 = 'bodor ullah';
+var address = "vooter goli kocu khet";
+var gf = 'alia bhat';
+
+
+var pass = true;
+var subscribed = false;
