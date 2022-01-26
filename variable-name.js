1937ce55a6ea9e6e2c30fa7cc385179f7ddf28d2
Bondhan Mondal <bondhan2725@gmail.com>
2022-01-26 17:45:04 +0600

    variable name naming convention done

---
+++ variable-name.js
@@ -0,0 +1,26 @@
+var name = 'Hero Alan';
+
+
+// reserved keyword not allowes 
+var false = 15;
+var delete = 'delete';
+
+// name should be one word 
+var nayok name ='Tom Hanks';
+
+// quote is mot allowed 
+var "nayika" = "sabnur";
+
+var myofficeaddress = 'andorkilla bandarban';
+var my-office-address = 'andorkilla bandarban';
+var my_officea_ddress = 'andorkilla bandarban';
+var myOfficeAddress = 'andorkilla bandarban';
+
+var PIE = 3.14;
+
+
+var 99price = 25;
+var price99 = 96;
+
+var userHomeAddress = 'katabon';
+var uha = "katabon";
