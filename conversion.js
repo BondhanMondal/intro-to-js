44e5b4be1684795bd85000ac6649a0b54b9481e9
Bondhan Mondal <bondhan2725@gmail.com>
2022-01-26 19:17:41 +0600

    type conversion

---
+++ conversion.js
@@ -0,0 +1,30 @@
+var firstName = 'Jennie';
+var lastName = 'Kim';
+var fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+var fast = 'Arina';
+var last = 'Grande';
+var name = fast + ' ' + last;
+// console.log(name);
+
+var onionPrice = '42';
+var eggPrice = '41';
+
+var totalPrice = onionPrice + eggPrice;
+// console.log(totalPrice);
+
+// integer 
+var sunglass = 3;
+
+// float 
+var price = 99.98;
+
+var onionPrice = '42';
+var eggPrice = '41.58';
+
+var onionPriceNumber = parseInt(onionPrice);
+var eggPriceNumber = parseFloat(eggPrice);
+console.log(onionPriceNumber);
+console.log(onionPrice);
+console.log(eggPriceNumber);
